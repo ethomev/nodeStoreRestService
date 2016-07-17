3b3f9d51e81a998721d4c3b757a26010e0405db2
Thomas Melville <thomas.melville@ericsson.com>
2016-07-17 14:29:44 +0100

    Category HTTP Methods Operations
    * Get Single Category
    * Post Category

--- src/app/config/category.js
+++ src/app/config/category.js
@@ -3,7 +3,6 @@ var assert = require('assert');
 
 var url = "mongodb://localhost:27017/music_store";
 
-
 module.exports.getAll = function(req, res){
   mongo.connect(url, function(err, db){
     assert.equal(null, err);
@@ -18,4 +17,39 @@ module.exports.getAll = function(req, res){
 
     db.close();
   });
+};
+
+module.exports.getSingle = function(req, res){
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    console.log("Successfully connected to db");
+
+    var name = req.params.name;
+    var collections = db.collection("categories");
+
+    collections.find({name:name}).toArray(function(err,docs){
+      assert.equal(null,err);
+      res.send(docs);
+    });
+
+    db.close();
+  })
+};
+
+module.exports.postCategory = function(req, res){
+  mongo.connect(url, function(err, db){
+      assert.equal(null, err);
+      console.log("Successfully connected to db");
+      var category = {
+        name:req.body.name,
+        desc:req.body.desc,
+        parent:req.body.parent};
+      var collections = db.collection("categories");
+
+      collections.insert(category, function(err, result){
+        assert.equal(null, err);
+        res.send("Ok");
+        });
+      db.close();
+    })
 };
--- src/app/config/server.js
+++ src/app/config/server.js
@@ -1,9 +1,15 @@
 // This will be the entry point for the service
 var express = require('express');
 var category = require('./category');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json())
+
 app.get('/category', category.getAll)
+app.get('/category/:name', category.getSingle)
+
+app.post('/category', category.postCategory)
 
 app.listen(3000, function(){
   console.log('Listening on port 3000');
